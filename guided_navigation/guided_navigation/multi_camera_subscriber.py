3cfcda41c391d6289bd03ca0bdee891fd41526b5
GDanielf <gudanielf@gmail.com>
2024-09-03 18:47:26 -0300

    Add files via upload

---
+++ guided_navigation/guided_navigation/multi_camera_subscriber.py
@@ -0,0 +1,53 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+
+class MultiCameraSubscriber(Node):
+    def __init__(self):
+        super().__init__('multi_camera_subscriber')
+        
+        # Subscrições para os três tópicos de imagem
+        self.subscription1 = self.create_subscription(
+            Image,
+            '/world/empty/model/rgbd_camera/link/link/sensor/camera_sensor/image',
+            self.camera1_callback,
+            10)
+        
+        self.subscription2 = self.create_subscription(
+            Image,
+            '/world/empty/model/rgbd_camera_1/link/link_1/sensor/camera_sensor_1/image',
+            self.camera2_callback,
+            10)
+        
+        self.subscription3 = self.create_subscription(
+            Image,
+            '/world/empty/model/rgbd_camera_2/link/link_2/sensor/camera_sensor_2/image',
+            self.camera3_callback,
+            10)
+        
+        # Desativar mensagens de retorno de chamada não utilizadas
+        self.subscription1
+        self.subscription2
+        self.subscription3
+
+    def camera1_callback(self, msg):
+        self.get_logger().info('Recebida imagem da câmera 1')
+        # Processamento da imagem da câmera 1
+
+    def camera2_callback(self, msg):
+        self.get_logger().info('Recebida imagem da câmera 2')
+        # Processamento da imagem da câmera 2
+
+    def camera3_callback(self, msg):
+        self.get_logger().info('Recebida imagem da câmera 3')
+        # Processamento da imagem da câmera 3
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MultiCameraSubscriber()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ guided_navigation/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'guided_navigation'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='gdaniel',
+    maintainer_email='gudanielf@gmail.com',
+    description='Guided navigation project',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'multi_camera = guided_navigation.multi_camera_subscriber:main'
+        ],
+    },
+)
