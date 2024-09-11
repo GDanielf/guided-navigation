e63dc7fddb91c20d1f46395856ade0fd2147f8ae
GDanielf <gudanielf@gmail.com>
2024-09-10 22:28:45 -0300

    Add files via upload

--- guided_navigation/scripts/multi_camera_subscriber.py
+++ guided_navigation/scripts/multi_camera_subscriber.py
@@ -2,13 +2,20 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
+from roboflow import Roboflow
 import cv2
-import time
+import inference
 
 class MultiCameraSubscriber(Node):
     def __init__(self):
         super().__init__('multi_camera_subscriber')
-        
+
+        self.image = None
+        self.bridge = CvBridge()
+
+        # Configuração do Modelo
+        self.model = inference.get_model("husky_test/3")
+
         # Subscrições para os três tópicos de imagem
         self.subscription1 = self.create_subscription(
             Image,
@@ -41,9 +48,29 @@ class MultiCameraSubscriber(Node):
         self.subscription3
         self.subscription4
 
+
     def camera1_callback(self, msg):
         self.get_logger().info('Recebida imagem da câmera 1')
         # Processamento da imagem da câmera 1
+        cv_image = self.bridge.imgmsg_to_cv2(msg, 'bgr8')
+        predict = self.model.infer(image = cv_image)
+        print(predict)
+        #predict_image = cv2.resize(predict, (640, 480))
+        for prediction in predict[0].predictions:
+            # Extrai as coordenadas e dimensões da caixa delimitadora
+            x = int(prediction.x)
+            y = int(prediction.y)
+            width = int(prediction.width)
+            height = int(prediction.height)
+            
+            # Calcula as coordenadas dos cantos da caixa
+            top_left = (abs(int(width/2) - x), abs(int(height/2) - y))
+            bottom_right = (int(width/2) + x, int(height/2) + y)
+            
+            # Desenha a caixa delimitadora na imagem
+            cv2.rectangle(cv_image, top_left, bottom_right, (0, 255, 0), 2)  # Verde, espessura 2
+        cv2.imshow('Camera 1', cv_image)
+        cv2.waitKey(1)       
 
     def camera2_callback(self, msg):
         self.get_logger().info('Recebida imagem da câmera 2')
@@ -62,7 +89,9 @@ def main(args=None):
     node = MultiCameraSubscriber()
     rclpy.spin(node)
     node.destroy_node()
+    cv2.destroyAllWindows()
     rclpy.shutdown()
+    
 
 if __name__ == '__main__':
     main()
